6bae4f093df1a5a6fd2a9fe7d62deb7ef7b14c2a
luojia65 <me@luojia.cc>
2021-06-07 02:31:38 +0800

    SXSTATUS and SHCR registers

--- src/register/shcr.rs
+++ src/register/shcr.rs
@@ -1,13 +1,13 @@
-//! mhcr, machine hardware configuration register
+//! shcr, supervisor hardware configuration register
 use bit_field::BitField;
 
-/// mhcr register
+/// shcr register
 #[derive(Clone, Copy, Debug)]
-pub struct Mhcr {
+pub struct Shcr {
     bits: usize,
 }
 
-impl Mhcr {
+impl Shcr {
     /// I-cache enable
     #[inline]
     pub fn ie(&self) -> bool {
@@ -50,39 +50,6 @@ impl Mhcr {
     }
 }
 
-set!(0x7C1);
-clear!(0x7C1);
-read_csr_as!(Mhcr, 0x7C1);
-
-set_clear_csr! {
-    /// I-cache enable
-    , set_ie, clear_ie, 1 << 0
-}
-set_clear_csr! {
-    /// D-cache enable
-    , set_de, clear_de, 1 << 1
-}
-set_clear_csr! {
-    /// Cache write allocate configuration enable
-    , set_wa, clear_wa, 1 << 2
-}
-set_clear_csr! {
-    /// Write back enable; clear this bit to be write through
-    , set_wb, clear_wb, 1 << 3
-}
-set_clear_csr! {
-    /// Return stack enable
-    , set_rs, clear_rs, 1 << 4
-}
-set_clear_csr! {
-    /// Branch predict enable
-    , set_bpe, clear_bpe, 1 << 5
-}
-set_clear_csr! {
-    /// Branch target buffer enable
-    , set_btb, clear_btb, 1 << 6
-}
-set_clear_csr! {
-    /// Write bulk transfer enable
-    , set_wbr, clear_wbr, 1 << 8
-}
+set!(0x5C1);
+clear!(0x5C1);
+read_csr_as!(Shcr, 0x5C1);
---
+++ src/register/sxstatus.rs
@@ -0,0 +1,82 @@
+//! sxstatus, supervisor extended status register
+
+use bit_field::BitField;
+pub use super::mxstatus::PM;
+
+/// sxstatus register
+#[derive(Clone, Copy, Debug)]
+pub struct Sxstatus {
+    bits: usize,
+}
+
+impl Sxstatus {
+    /// User mode performance monitor enable
+    #[inline]
+    pub fn pmdu(&self) -> bool {
+        self.bits.get_bit(10)
+    }
+    /// Supervisor mode performance monitor enable
+    #[inline]
+    pub fn pmds(&self) -> bool {
+        self.bits.get_bit(11)
+    }
+    /// Machine mode performance monitor enable
+    #[inline]
+    pub fn pmdm(&self) -> bool {
+        self.bits.get_bit(12)
+    }
+    /// Is PMP minimum stride 4K bytes
+    #[inline]
+    pub fn pmp4k(&self) -> bool {
+        self.bits.get_bit(14)
+    }
+    /// Unaligned access enable
+    #[inline]
+    pub fn mm(&self) -> bool {
+        self.bits.get_bit(15)
+    }
+    /// User mode allow extended cache instruction
+    #[inline]
+    pub fn ucme(&self) -> bool {
+        self.bits.get_bit(16)
+    }
+    /// CLINT supervisor extension enable
+    #[inline]
+    pub fn clintee(&self) -> bool {
+        self.bits.get_bit(17)
+    }
+    /// Hardware refill when TLB item absent enable
+    #[inline]
+    pub fn mhrd(&self) -> bool {
+        self.bits.get_bit(18)
+    }
+    /// Extend MMU page table entry address attributes enable
+    #[inline]
+    pub fn maee(&self) -> bool {
+        self.bits.get_bit(21)
+    }
+    /// T-Head extended instruction set architecture enable
+    #[inline]
+    pub fn theadisaee(&self) -> bool {
+        self.bits.get_bit(22)
+    }
+    /// Current privileged mode
+    #[inline]
+    pub fn pm(&self) -> PM {
+        match self.bits.get_bits(30..=31) {
+            0b00 => PM::User,
+            0b01 => PM::Supervisor,
+            0b11 => PM::Machine,
+            _ => unreachable!(),
+        }
+    }
+}
+
+set!(0x5C0);
+clear!(0x5C0);
+read_csr_as!(Sxstatus, 0x5C0);
+
+set_clear_csr! {
+    /// Unaligned access enable
+    , set_mm, clear_mm, 1 << 15
+}
