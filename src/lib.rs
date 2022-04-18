896afde44e9534bf24fecbafc43cb000ca9e47a6
luojia65 <me@luojia.cc>
2022-04-18 17:40:37 +0800

    doc: enable and invalidate caches

--- src/lib.rs
+++ src/lib.rs
@@ -32,30 +32,35 @@
 //! unsafe { transmute::<_, fn()>(insn_ptr)() };
 //! ```
 //!
-//! ## Enable and disable I-cache
+//! ## Enable and invalidate caches
 //!
-//! C906 will invalid all I-cache automatically when reset; use the following operation to enable I-cache.
-//! 
-//! ```no_run
-//! // If we don't have `mxstatus.theadisaee` enabled
-//! use xuantie::register::mcor::{self, Cache, Operation};
-//! unsafe { 
-//!     mcor::cache(Cache::BOTH, Operation::INVALIDATE);
-//! }
-//! // If we have `mxstatus.theadisaee` enabled
-//! use xuantie::asm::{icache_iall, sync_is};
-//! unsafe {
-//!     icache_iall();
-//!     sync_is();
-//! }
-//! ```
-//!
-//! Additionally, we can disable I-cache manually if necessary.
+//! C906 will invalid both I-cache and D-cache automatically when reset.
+//! Use the following operation, we can enable I-cache and D-cache.
 //!
 //! ```no_run
 //! use xuantie::register::mhcr;
 //! // enable D-cache
-//! unsafe { mhcr::clear_de() };
+//! unsafe { mhcr::set_de() };
+//! // enable I-cache
+//! unsafe { mhcr::set_ie() };
+//! ```
+//!
+//! Additionally, we can invalidate I-cache or D-cache manually if necessary.
+//!
+//! ```no_run
+//! // If we don't have `mxstatus.theadisaee` enabled
+//! use xuantie::register::mcor::{self, Cache, Operation};
+//! // invalidate caches. Use `Cache::DATA` or `Cache::INSTRUCION`
+//! // to invalid a specific cache.
+//! unsafe { mcor::cache(Cache::BOTH, Operation::INVALIDATE); }
+//! ```
+//! ```no_run
+//! // If we have `mxstatus.theadisaee` enabled
+//! use xuantie::asm::{icache_iall, dcache_iall, sync_is};
+//! // invalidate I-cache
+//! unsafe { icache_iall(); sync_is(); }
+//! // invalidate D-cache
+//! unsafe { dcache_iall(); sync_is(); }
 //! ```
 //!
 //! ## CPU power down
