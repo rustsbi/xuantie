12a27abc044a09be96bafcafc37aaeda3a807bf5
Zhouqi Jiang <luojia@hust.edu.cn>
2024-12-12 17:13:27 +0800

    rt: add interrupt dispatcher symbol
    The `__CORE_INTERRUPTS` enum definition is merged from C906, C907, C908, C910 and C920 core documents.
    
    Signed-off-by: Zhouqi Jiang <luojia@hust.edu.cn>

---
+++ xuantie-riscv-rt/src/interrupts.rs
@@ -0,0 +1,50 @@
+//! Interrupt handling for Xuantie RISC-V interrupt handling standard.
+
+unsafe extern "C" {
+    fn SupervisorSoft();
+    fn MachineSoft();
+    fn SupervisorTimer();
+    fn MachineTimer();
+    fn SupervisorExternal();
+    fn MachineExternal();
+}
+
+unsafe extern "C" {
+    fn THeadPmuOverflow();
+    fn THeadEccBusError();
+}
+
+pub static __CORE_INTERRUPTS: [Option<unsafe extern "C" fn()>; 18] = [
+    None,
+    Some(SupervisorSoft),
+    None,
+    Some(MachineSoft),
+    None,
+    Some(SupervisorTimer),
+    None,
+    Some(MachineTimer),
+    None,
+    Some(SupervisorExternal),
+    None,
+    Some(MachineExternal),
+    None,
+    Some(THeadPmuOverflow),
+    None,
+    None,
+    Some(THeadEccBusError),
+    Some(THeadPmuOverflow),
+];
+
+// TODO: distinguish different supported core interrupts between c906, c908, etc. cores.
+
+#[inline]
+#[unsafe(no_mangle)]
+pub extern "C" fn _dispatch_core_interrupt(code: usize) {
+    unsafe extern "C" {
+        fn DefaultHandler();
+    }
+    match __CORE_INTERRUPTS.get(code) {
+        Some(Some(handler)) => unsafe { handler() },
+        _ => unsafe { DefaultHandler() },
+    }
+}
---
+++ xuantie-riscv-rt/src/lib.rs
@@ -0,0 +1,4 @@
+//! Startup code and minimal runtime for T-Head Xuantie RISC-V CPUs
+#![no_std]
+
+pub mod interrupts;
