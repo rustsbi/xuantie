5822d6f3d6402c3bc4db86fec1eae522c664d7b9
Zhouqi Jiang <luojia@rustsbi.com>
2026-02-06 14:54:20 +0800

    feat(riscv): asm: add sfence.vmas wrapper in xtheadsync
    Signed-off-by: Zhouqi Jiang <luojia@rustsbi.com>

--- xuantie-riscv/src/asm/xtheadsync.rs
+++ xuantie-riscv/src/asm/xtheadsync.rs
@@ -1,7 +1,5 @@
 use core::arch::asm;
 
-// TODO: th.sfence.vmas
-
 /// Synchronize instruction.
 ///
 /// Ensures that all instructions before retire earlier than this instruction,
@@ -96,4 +94,73 @@ pub unsafe fn sync_i() {
 pub unsafe fn sync_is() {
     // th.sync.is
     asm!(".insn i 0x0B, 0, x0, x0, 0x01B")
+}
+
+// TODO platform support
+
+/// Invalidate TLB on all harts via broadcasting for all address spaces and virtual addresses.
+///
+/// # Permissions
+///
+/// This instruction can be executed in all privilege levels higher than U mode. Attempts to execute
+/// this instruction in U mode raise an illegal instruction exception.
+///
+/// # Exceptions
+///
+/// This instruction does not trigger any exceptions.
+#[inline]
+pub unsafe fn sfence_vmas_all() {
+    // th.sfence.vmas x0, x0
+    asm!(
+        ".insn r 0x0B, 0, x0, x0, x0, 0x02",
+        options(nostack, preserves_flags)
+    )
+}
+
+/// Invalidate TLB on all harts via broadcasting for given virtual address.
+///
+/// # Permissions
+///
+/// This instruction can be executed in all privilege levels higher than U mode. Attempts to execute
+/// this instruction in U mode raise an illegal instruction exception.
+///
+/// # Exceptions
+///
+/// This instruction does not trigger any exceptions.
+#[inline]
+pub unsafe fn sfence_vmas_vaddr(vaddr: usize) {
+    // th.sfence.vmas vaddr, x0
+    asm!(".insn r 0x0B, 0, x0, {}, x0, 0x02", in(reg) vaddr, options(nostack, preserves_flags))
+}
+
+/// Invalidate TLB on all harts via broadcasting for given address space.
+///
+/// # Permissions
+///
+/// This instruction can be executed in all privilege levels higher than U mode. Attempts to execute
+/// this instruction in U mode raise an illegal instruction exception.
+///
+/// # Exceptions
+///
+/// This instruction does not trigger any exceptions.
+#[inline]
+pub unsafe fn sfence_vmas_asid(asid: usize) {
+    // th.sfence.vmas x0, asid
+    asm!(".insn r 0x0B, 0, x0, x0, {}, 0x02", in(reg) asid, options(nostack, preserves_flags))
+}
+
+/// Invalidate TLB on all harts via broadcasting for given virtual address and address space.
+///
+/// # Permissions
+///
+/// This instruction can be executed in all privilege levels higher than U mode. Attempts to execute
+/// this instruction in U mode raise an illegal instruction exception.
+///
+/// # Exceptions
+///
+/// This instruction does not trigger any exceptions.
+#[inline]
+pub unsafe fn sfence_vmas(vaddr: usize, asid: usize) {
+    // th.sfence.vmas vaddr, asid
+    asm!(".insn r 0x0B, 0, x0, {}, {}, 0x02", in(reg) vaddr, in(reg) asid, options(nostack, preserves_flags))
 }
